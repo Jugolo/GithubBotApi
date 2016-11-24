bf44555dd63626d143de747350d4f4ce88777380
Ronnie <rix17172@gmail.com>
2016-11-24 22:22:49 +0100

    Create GitHubBot.php

---
+++ php/GitHubBot/GitHubBot.php
@@ -0,0 +1,34 @@
+<?php
+namespace GitHubBot\GitHubBot;
+
+class GitHubBot{
+  private $config;
+
+  /**
+  *Set config to be used when it try to connect to cowscript server
+  */
+  public function setConfig(BotConfig $config){
+    $this->config = $config;
+  }
+
+  /**
+  *Get config for the bot.
+  *@throw Exception if the config not yet is set
+  *@return BotConfig
+  */
+  public function getConfig() : BotConfig{
+    if($this->config == null){
+      throw new \Exception("Missing bot config. Please use setConfig before getConfig");
+    }
+    return $this->config;
+  }
+
+  /**
+  *Start buildning event 
+  *@throw Exception when config is not yet set
+  *@return EventBuilder
+  */
+  public function getBuilder() : EventBuilder{
+    return new EventBuilder($this->getConfig());
+  }
+}
