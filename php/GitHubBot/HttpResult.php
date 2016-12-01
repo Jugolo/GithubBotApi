755b10acde52aa8c40cbdab59d2f7bd835a1556e
Ronnie <rix17172@gmail.com>
2016-12-02 00:00:32 +0100

    Update HttpResult.php

--- php/GitHubBot/HttpResult.php
+++ php/GitHubBot/HttpResult.php
@@ -3,13 +3,20 @@ namespace GitHub\HttpResult;
 
 class HttpResult{
   public $status;
+  public $head = [];
   
   public function __construct($socket){
     while($line = fgets($socket)){
       if($line == "")
         break;
+      if(strpos($line, "HTTP/1.1") === 0){
+        list($type, $status, $ok) = explode(" ", $line);
+        $this->status = $status;
+        continue;
+      }
       
-      
+      $pos = strpos($line, ":");
+      $this->head[substr($line, 0, $pos)] = substr($line, $pos+2);
     }
   }
 }
