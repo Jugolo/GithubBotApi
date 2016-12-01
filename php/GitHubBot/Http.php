fa419ea3ff6c208f7cf0cb88b29c0ad0dc10040d
Ronnie <rix17172@gmail.com>
2016-12-01 20:10:45 +0100

    Update Http.php

--- php/GitHubBot/Http.php
+++ php/GitHubBot/Http.php
@@ -2,5 +2,23 @@
 namespace GitHubBot\Http;
 
 class Http{
-
+  private $information = [
+    "ssl"  => false,
+    "host" => null,
+    "port" => 80,
+    "path" => "/"
+  ];
+  
+  public function __construct(string $url){
+    if(($pos = strpos($url, "://")) !== false){
+      if(substr($url, 0, $pos) == "https"){
+        $this->information["ssl"] = true;
+      }
+      $url = substr($url, $pos+3);
+    }
+    
+    if(strpos($url, "www.") === 0){
+      $url = substr($url, 4);
+    }
+  }
 }
