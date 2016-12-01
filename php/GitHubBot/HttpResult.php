21418bce51a929036302595274765a72eaaff1ec
Ronnie <rix17172@gmail.com>
2016-12-01 23:56:30 +0100

    Update HttpResult.php

--- php/GitHubBot/HttpResult.php
+++ php/GitHubBot/HttpResult.php
@@ -2,5 +2,14 @@
 namespace GitHub\HttpResult;
 
 class HttpResult{
-
+  public $status;
+  
+  public function __construct($socket){
+    while($line = fgets($socket)){
+      if($line == "")
+        break;
+      
+      
+    }
+  }
 }
